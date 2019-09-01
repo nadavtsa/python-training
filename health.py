a8d638820b9f84dbbd8ef973ea0d791bd6fbb9a6
Nadav Tsameret <nadavt@checkpoint.com>
2019-09-01 14:00:48 +0300

    deleted change1 branch

--- health.py
+++ health.py
@@ -1,9 +1,10 @@
 import random
-
+import hello_worls
 health = 50
 difficulty = random.randint(1, 3)
 potion_health = int(random.randint(25, 50) / difficulty)
 health = health + potion_health
 print(health)
+print(round(2.6))
 
 
---
+++ input.py
@@ -0,0 +1,7 @@
+def about(name, age):
+    sentence = "Meet {}, He/She is {} years old".format(name, age)
+    return sentence
+
+Nadav = {"name": "Nadav", "age": 25}
+
+print(sum((25, 26)))
