ff8067c7e54298dfaec9d096a552f4362912dfd1
Nadav Tsameret <nadavt@checkpoint.com>
2019-09-24 15:27:07 +0300

    saving final version

--- RESTful-with-SQL/RESTful.py
+++ RESTful-with-SQL/RESTful.py
@@ -1,14 +1,17 @@
 from datetime import timedelta
 
 from flask_restful import Api
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
-from user import UserRegister, UserResource, Users, UserLogin
+from user import UserRegister, UserResource, Users, UserLogin, TokenRefresh, UserLogout
 from item import Item, ItemList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.secret_key = 'nadav'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=180)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 api = Api(app)
 
 jwt = JWTManager(app)   # not creating the /auth endpoint
@@ -20,12 +23,44 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({'description': 'The token has expired',
+                    'error': 'token_expired'}), 401
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({'description': 'Signature verification failed',
+                    'error': 'Invalid token'}), 401
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(error):
+    return jsonify({'description': 'Request does not contain access token',
+                    'error': 'authorization required'}), 401
+
+@jwt.needs_fresh_token_loader
+def needs_fresh_token_callback():
+    return jsonify({'description': 'The token is not fresh',
+                    'error': 'Fresh token required'}), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({'description': 'The token has been revoked',
+                    'error': 'token_revoked'}), 401
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserResource, '/user/<int:user_id>')
 api.add_resource(Users, '/users')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 app.run(port=5000, debug=True)
 
--- RESTful-with-SQL/user.py
+++ RESTful-with-SQL/user.py
@@ -1,7 +1,10 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_claims, jwt_refresh_token_required, get_jwt_identity
+from blacklist import BLACKLIST
+from flask_jwt_extended import (
+    create_access_token, create_refresh_token, jwt_required, get_jwt_claims, jwt_refresh_token_required, get_jwt_identity,
+    get_raw_jwt)
 
 
 _user_parser = reqparse.RequestParser()
@@ -126,8 +129,16 @@ class UserLogin(Resource):
         return {'message': 'Invalid credentials'}, 401
 
 
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully logged out'}, 200
+
 class TokenRefresh(Resource):
     @jwt_refresh_token_required
     def post(self):
         curr_user = get_jwt_identity()
         new_token = create_access_token(identity=curr_user, fresh=False)
+        return {'access_token': new_token}, 200
